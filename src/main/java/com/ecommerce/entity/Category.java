fb5038307bc5fa8d34aeed66b1c431766668f8c1
hthien <leviethoangthien2005@gmail.com>
2026-07-30 10:47:35 +0700

    feat: add Category and Product entities with JPA repositories

---
+++ src/main/java/com/ecommerce/entity/Category.java
@@ -0,0 +1,25 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Category name is required")
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
