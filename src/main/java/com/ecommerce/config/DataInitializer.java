0df2737eb17a847cefbf1f1d51379669a09e4153
hthien <leviethoangthien2005@gmail.com>
2026-08-06 19:28:39 +0700

    refactor: limit just run in dev/local

--- src/main/java/com/ecommerce/config/DataInitializer.java
+++ src/main/java/com/ecommerce/config/DataInitializer.java
@@ -6,8 +6,10 @@ import com.ecommerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile("dev")
 @Configuration
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
