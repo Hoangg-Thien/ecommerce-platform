7ab41f9ef5901bdfe2ed72ef4a42f7cae99d2b09
hthien <leviethoangthien2005@gmail.com>
2026-08-06 12:23:12 +0700

    feat: add OrderMapper and OrderRepository for order processing

---
+++ src/main/java/com/ecommerce/mapper/OrderMapper.java
@@ -0,0 +1,56 @@
+package com.ecommerce.mapper;
+
+import com.ecommerce.repository.OrderRepository;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.dto.response.OrderItemResponse;
+import com.ecommerce.dto.response.OrderResponse;
+import com.ecommerce.entity.Order;
+import com.ecommerce.entity.OrderItem;
+
+@Component
+public class OrderMapper {
+    private final OrderRepository orderRepository;
+
+    OrderMapper(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public OrderItemResponse tOrderItemResponse(OrderItem orderItem){
+        if(orderItem == null){
+            return null;
+        }
+
+        BigDecimal subTotal = orderItem.getPrice().multiply(BigDecimal.valueOf(orderItem.getQuantity()));
+
+        return OrderItemResponse.builder()
+        .id(orderItem.getId())
+        .productId(orderItem.getProduct().getId())
+        .productName(orderItem.getProduct().getName())
+        .quantity(orderItem.getQuantity())
+        .price(orderItem.getPrice())
+        .subTotal(subTotal)
+        .build();
+    }
+
+    public OrderResponse tOrderResponse(Order order){
+        if(order == null){
+            return null;
+        }
+
+        List<OrderItemResponse> itemsResponses = order.getItems().stream()
+        .map(this::tOrderItemResponse)
+        .toList();
+
+        return OrderResponse.builder()
+        .id(order.getId())
+        .id(order.getUser().getId())
+        .status(order.getStatus())
+        .totalPrice(order.getTotalPrice())
+        .items(itemsResponses)
+        .build();
+    }
+}
