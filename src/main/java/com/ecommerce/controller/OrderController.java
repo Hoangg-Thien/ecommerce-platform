100d1b70806d01e51796f787189275fd21c30182
hthien <leviethoangthien2005@gmail.com>
2026-08-06 13:20:25 +0700

    feat: implement order creation functionality with full layers

---
+++ src/main/java/com/ecommerce/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.ecommerce.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.dto.response.OrderResponse;
+import com.ecommerce.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<OrderResponse> createOrder(
+        @AuthenticationPrincipal UserDetails userDetails
+    ){
+        OrderResponse orderResponse = orderService.createOrder(userDetails.getUsername());
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
+    }
+}
