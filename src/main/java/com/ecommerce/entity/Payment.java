3cb7ad7c9d09f4ace798d6f8c1c860dc58ff40c4
hthien <leviethoangthien2005@gmail.com>
2026-08-06 20:38:13 +0700

    feat: implement Payment entity and PaymentStatus enum for payment processing

---
+++ src/main/java/com/ecommerce/entity/Payment.java
@@ -0,0 +1,51 @@
+package com.ecommerce.entity;
+
+import java.lang.annotation.ElementType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.ecommerce.enums.PaymentMethod;
+import com.ecommerce.enums.PaymentStatus;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Payment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod; 
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;  
+
+    @Column(nullable = false)
+    private BigDecimal amount; // order.totalPrice
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
